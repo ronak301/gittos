8c27d6e5f70ad00687f88b70117c95da6139bbfd
rkothari <ronak.kothari@sprinklr.com>
2017-05-15 03:17:05 +0530

    adding eslint-plugin-import

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SeachRepo from './components/SearchRepo';
 import './index.css';
 
+const App = () => (
+  <div >
+    <div>
+      <h2>Welcome to React</h2>
+    </div>
+    <p >
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
+  </div>
+);
+
 ReactDOM.render(
-  <SeachRepo />,
+  <App />,
   document.getElementById('root')
 );
