0e83d5bc0d09e860e64a7c61898b7b6903e7c7ab
rkothari <ronak.kothari@sprinklr.com>
2017-05-16 00:57:10 +0530

    adding react table

--- src/components/SearchRepo/SearchRepo.js
+++ src/components/SearchRepo/SearchRepo.js
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { get as _get, debounce as _debounce } from 'lodash';
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
 
 import './SearchRepo.css'
 
@@ -38,45 +40,37 @@ class SearchRepo extends Component {
 
 
   renderTable = () => {
-    return (
-      <table>
-        <tr>
-          <th>Company</th>
-          <th>Contact</th>
-          <th>Country</th>
-        </tr>
-        <tr>
-          <td>Alfreds Futterkiste</td>
-          <td>Maria Anders</td>
-          <td>Germany</td>
-        </tr>
-        <tr>
-          <td>Centro comercial Moctezuma</td>
-          <td>Francisco Chang</td>
-          <td>Mexico</td>
-        </tr>
-        <tr>
-          <td>Ernst Handel</td>
-          <td>Roland Mendel</td>
-          <td>Austria</td>
-        </tr>
-        <tr>
-          <td>Island Trading</td>
-          <td>Helen Bennett</td>
-          <td>UK</td>
-        </tr>
-        <tr>
-          <td>Laughing Bacchus Winecellars</td>
-          <td>Yoshi Tannamuri</td>
-          <td>Canada</td>
-        </tr>
-        <tr>
-          <td>Magazzini Alimentari Riuniti</td>
-          <td>Giovanni Rovelli</td>
-          <td>Italy</td>
-        </tr>
-      </table>
-    );
+    const data = [{
+      name: 'Tanner Linsley',
+      age: 26,
+      friend: {
+        name: 'Jason Maurer',
+        age: 23,
+      }
+    }]
+
+    const columns = [{
+        Header: 'Name',
+        accessor: 'name' // String-based value accessors!
+      }, {
+        Header: 'Age',
+        accessor: 'age',
+        Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+      }, {
+        id: 'friendName', // Required because our accessor is not a string
+        Header: 'Friend Name',
+        accessor: d => d.friend.name // Custom value accessors!
+      }, {
+        Header: props => <span>Friend Age</span>, // Custom header components!
+        accessor: 'friend.age'
+      }]
+
+      return (
+        <ReactTable
+          data={data}
+          columns={columns}
+        />
+      )
   }
 
 //  handleChange = _debounce( (event) => {
